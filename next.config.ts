a9d9420d8d0ba45fa699f4fba3c50c97eb8f3eea
Gary <andresfelipefandinocaceres@gmail.com>
2025-11-18 14:02:09 -0500

    Added image routing 'whitelable'

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://api.cacolombia.com/v1/**")
+    ]
+  },
+  allowedDevOrigins: [
+    "test.cacolombia.com",
+    "*.cacolombia.com",
+  ]
 };
 
 export default nextConfig;
